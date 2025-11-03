1df0493ccaa0629cc9f07aa999cf014565d4cf8d
santhosh <santhoshskp434@gmail.com>
2025-11-03 16:24:06 +0000

    workflow dispatch added

--- .github/workflows/script/app_validate.sh
+++ .github/workflows/script/app_validate.sh
@@ -2,11 +2,10 @@
 
 # Function to deploy using Azure CLI
 validate_runner() {
-    local uuid="$(cat /proc/sys/kernel/random/uuid)"
     local parameters_file="Deployments/$1/$1.json"
     local template_file="$2"
 
-    az deployment group validate --name "$uuid" --resource-group apim-rg --template-file "$template_file" --parameters "$parameters_file"
+    az deployment group validate --resource-group apim-rg --template-file "$template_file" --parameters "$parameters_file"
 }
 
 # Main script starts here
