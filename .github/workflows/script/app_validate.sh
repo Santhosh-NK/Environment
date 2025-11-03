297799baf4c17caec0537762933433fbf03c8355
santhosh <santhoshskp434@gmail.com>
2025-11-03 16:17:39 +0000

    workflow dispatch added

--- .github/workflows/script/app_validate.sh
+++ .github/workflows/script/app_validate.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-
 # Function to deploy using Azure CLI
 validate_runner() {
     local uuid="$(cat /proc/sys/kernel/random/uuid)"
-    local app_name="Deployments/$1/$1.json"
+    local parameters_file="Deployments/$1/$1.json"
+    local template_file="$2"
 
-
-
-            az deployment group validate --name $uuid --resource-group apim-rg --template-file $template_file 
-            az deployment group what-if --name $uuid --resource-group apim-rg --template-file $template_file 
-    
-            
-
+    az deployment group validate --name "$uuid" --resource-group apim-rg --template-file "$template_file" --parameters "$parameters_file"
 }
 
 # Main script starts here
 template_file="./app_service.bicep"
-apps = ("app1", "app2")
-for app in "{$apps[@]}"; do 
-    validate_runner "$app" 
-done
+apps=("app1" "app2")
 
+for app in "${apps[@]}"; do 
+    validate_runner "$app" "$template_file"
+done
